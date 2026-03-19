079cf77483e22acf7f2d38ca5e57adcb2d0e6c5c
kassi-on <kassioadrianosilva@gmail.com>
2026-03-18 22:30:52 -0300

    a

--- velilha.c
+++ velilha.c
@@ -5,4 +5,14 @@ int main(){
     char p1='1', p2='2', p3='3', p4='4', p5='5', p6='6', p7='7', p8='8', p9='9';
 
     char jogador = 'X'; 
+    int jogadas = 0;
+    int ganhou = 0;
+    int escolha;
+
+    //desenho do tabuleiro
+    printf("\n JOGO DA VELHA \n\n");
+    printf ("%c | %c | %c \n",  p1, p2, p3);
+    printf("----|----|----\n");
+    printf(" %c  | %c | %c \n", p4, p5, p6");
+
 }
